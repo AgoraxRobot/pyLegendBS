b1a6313c825ca8c156f8db38d05512129075f2ff
Team Agora <132120521+TeamAgora@users.noreply.github.com>
2023-05-24 23:33:31 +0530

    Update start.py

--- LegendBS/start.py
+++ LegendBS/start.py
@@ -5,24 +5,24 @@ async def start_cmd(Legend):
     START_OP = [
         [
             InlineKeyboardButton(
-                text="🥀 Developer 🥀", url=f"https://t.me/LegendSpamBot_Owner"
+                text="🥀 Developer 🥀", url=f"https://t.me/MR_AGORA"
             ),
             InlineKeyboardButton(
-                text="✨ Support ✨", url=f"https://t.me/LegendBotSpam"
+                text="✨ Support ✨", url=f"https://t.me/AGORAWORLD"
             ),
         ],
         [
             InlineKeyboardButton(
                 text="🧸 Add me in your group 🧸",
-                url=f"https://t.me/{x.username}?startgroup=true",
+                url=f"https://t.me/Agoraxrobot?startgroup=true",
             ),
         ],
         [
             InlineKeyboardButton(
-                text="❄️ Source Code ❄️", url=f"https://github.com/LEGEND-AI/BOTSPAM"
+                text="❄️ Source Code ❄️", url=f"https://t.me/kimjikoin_bot?startgroup=true"
             ),
             InlineKeyboardButton(
-                text="☁️ Updates ☁️", url=f"https://t.me/LegendBot_AI"
+                text="☁️ Updates ☁️", url=f"https://t.me/teamagora"
             ),
         ],
     ]
